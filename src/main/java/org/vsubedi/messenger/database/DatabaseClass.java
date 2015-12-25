773d6bc224cefe5caf84df06a2988d31761e3a24
Vivek Subedi <shree665@gmail.com>
2015-12-25 10:52:30 -0500

    first commit

---
+++ src/main/java/org/vsubedi/messenger/database/DatabaseClass.java
@@ -0,0 +1,23 @@
+package org.vsubedi.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.vsubedi.messenger.model.Message;
+import org.vsubedi.messenger.model.Profile;
+
+public class DatabaseClass {
+
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<String, Profile> profiles = new HashMap<>();
+
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	
+	public static Map<String, Profile> getProfiles() {
+		return profiles;
+	}
+
+}
---
+++ src/main/java/org/vsubedi/messenger/exception/DataNotFoundException.java
@@ -0,0 +1,12 @@
+package org.vsubedi.messenger.exception;
+
+public class DataNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = -6328286661536343936L;
+
+	
+	public DataNotFoundException(String message) {
+		super(message);
+	}
+	
+}
